0cffdaf24ca4ca643b7ee5e727b7de597a6e9990
Ricardo Braga <rcardiobraga@gmail.com>
2022-02-25 04:05:00 -0300

    alteracao na rota /

--- pages/index.jsx
+++ pages/index.jsx
@@ -112,7 +112,7 @@ export default function Home() {
 
       <main className={styles.main}>
       <div className={styles.formContainer}>
-                <form action="https://app-agenda-frontend.vercel.app/" method="post" encType='multipart/form-data'>
+                <form action="https://app-agenda-backend.herokuapp.com/" method="post" encType='multipart/form-data'>
                 <img className={styles.formImage} src='/imgs/avatarUpload.png' alt="Upload Image"/>
                 <div className={styles.imagePath}>{imageName}</div> 
                 <label className={styles.uploadLabel} htmlFor='file' >Selecionar Foto</label>
